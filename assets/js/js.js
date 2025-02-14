284ee0bd012e9eededfb969f9e81bf08b3b2731d
Ram Krishna Yadav <rkpy4234@gmail.com>
2025-02-14 13:45:25 +0545

    first

---
+++ assets/js/js.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const heartContainer = document.body;
+    function createHeart() {
+        const heart = document.createElement("div");
+        heart.classList.add("falling-heart");
+        heart.innerHTML = "<i class='fas fa-heart'></i>";
+        heart.style.left = Math.random() * 100 + "vw";
+        heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+        heartContainer.appendChild(heart);
+        setTimeout(() => {
+            heart.remove();
+        }, 5000);
+    }
+    setInterval(createHeart, 500);
+});
